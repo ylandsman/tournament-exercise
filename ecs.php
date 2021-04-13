f53dfd327d335074f3dcc29cc17ba8407d307c28
Artem Onyshchenko <temafey@gmail.com>
2021-04-13 22:00:37 +0300

    feature(test-exercise): create test project
    - Add ecs.php and phpstan.neon configs

---
+++ ecs.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/clean-code.php');
+    $containerConfigurator->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/php71.php');
+    $containerConfigurator->import(__DIR__ . '/vendor/symplify/easy-coding-standard/config/set/psr12.php');
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set('skip', []);
+};
